9299f2cfbbdbba427e9c078cedfcb12baa212c66
Oumayma Ben Jha <oben-jha@e3r7p14.1337.ma>
2025-07-06 19:12:45 +0100

    norminetted

--- TOKENIZER/operators.c
+++ TOKENIZER/operators.c
@@ -1,30 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operators.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oben-jha <oben-jha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 19:11:58 by oben-jha          #+#    #+#             */
+/*   Updated: 2025/07/06 19:11:58 by oben-jha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parsing.h"
 
 t_token	*get_operator_token(char *line, int *i)
 {
 	if (line[*i] == '|')
-	{
-		(*i)++;
-		return (create_token(TOKEN_PIPE, "|"));
-	}
+		return ((*i)++, create_token(TOKEN_PIPE, "|"));
 	if (line[*i] == '>')
 	{
 		(*i)++;
 		if (line[*i] == '>')
-		{
-			(*i)++;
-			return (create_token(TOKEN_REDIR_APPEND, ">>"));
-		}
+			return ((*i)++, create_token(TOKEN_REDIR_APPEND, ">>"));
 		return (create_token(TOKEN_REDIR_OUT, ">"));
 	}
 	if (line[*i] == '<')
 	{
 		(*i)++;
 		if (line[*i] == '<')
-		{
-			(*i)++;
-			return (create_token(TOKEN_REDIR_HEREDOC, "<<"));
-		}
+			return ((*i)++, create_token(TOKEN_REDIR_APPEND, "<<"));
 		return (create_token(TOKEN_REDIR_IN, "<"));
 	}
 	return (NULL);
