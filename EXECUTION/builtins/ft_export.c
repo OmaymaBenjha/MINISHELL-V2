a7166dd2b0543fc4611625ae62695af775134c28
unknown <ziane.aymn@gmail.com>
2025-07-15 05:47:32 +0100

    check if exported identifier is valid

--- EXECUTION/builtins/ft_export.c
+++ EXECUTION/builtins/ft_export.c
@@ -1,5 +1,21 @@
 #include "builtins.h"
 
+static int	is_valid_identifier(const char *s)
+{
+	int	i;
+
+	if (!s || (!ft_isalpha(s[0]) && s[0] != '_'))
+		return (0);
+	i = 1;
+	while (s[i] && s[i] != '=')
+	{
+		if (!ft_isalnum(s[i]) && s[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 static int	print_exported_vars(char **envp)
 {
 	int		i;
@@ -27,49 +43,55 @@ static int	print_exported_vars(char **envp)
 	return (0);
 }
 
+static int	add_new_env_var(char *arg, char ***envp_ptr, int count)
+{
+	char	**new_envp;
+	int		i;
+
+	new_envp = malloc(sizeof(char *) * (count + 2));
+	if (!new_envp)
+		return (1);
+	i = -1;
+	while (++i < count)
+		new_envp[i] = (*envp_ptr)[i];
+	new_envp[i] = ft_strdup(arg);
+	if (!new_envp[i])
+	{
+		free(new_envp);
+		return (1);
+	}
+	new_envp[i + 1] = NULL;
+	free(*envp_ptr);
+	*envp_ptr = new_envp;
+	return (0);
+}
+
 static int	add_or_update_env(char *arg, char ***envp_ptr)
 {
 	int		i;
-	int		count;
-	char	**new_envp;
 	size_t	len;
 
 	len = 0;
 	while (arg[len] && arg[len] != '=')
 		len++;
 	i = 0;
-	while ((*envp_ptr)[i]
-		&& (ft_strncmp((*envp_ptr)[i], arg, len) || (*envp_ptr)[i][len] != '='))
-		i++;
-	if ((*envp_ptr)[i])
+	while ((*envp_ptr)[i])
 	{
-		free((*envp_ptr)[i]);
-		(*envp_ptr)[i] = ft_strdup(arg);
-		if (!(*envp_ptr)[i])
-			return (1);
-	}
-	else
-	{
-		count = 0;
-		while ((*envp_ptr)[count])
-			count++;
-		new_envp = malloc(sizeof(char *) * (count + 2));
-		if (!new_envp)
-			return (1);
-		i = -1;
-		while (++i < count)
-			new_envp[i] = (*envp_ptr)[i];
-		new_envp[i] = ft_strdup(arg);
-		if (!new_envp[i])
+		if (ft_strncmp((*envp_ptr)[i], arg, len) == 0
+			&& ((*envp_ptr)[i][len] == '=' || (*envp_ptr)[i][len] == '\0'))
 		{
-			free(new_envp);
-			return (1);
+			if (arg[len] == '=')
+			{
+				free((*envp_ptr)[i]);
+				(*envp_ptr)[i] = ft_strdup(arg);
+				if (!(*envp_ptr)[i])
+					return (1);
+			}
+			return (0);
 		}
-		new_envp[i + 1] = NULL;
-		free(*envp_ptr);
-		*envp_ptr = new_envp;
+		i++;
 	}
-	return (0);
+	return (add_new_env_var(arg, envp_ptr, i));
 }
 
 int	ft_export(char **args, t_shell *shell)
@@ -83,15 +105,20 @@ int	ft_export(char **args, t_shell *shell)
 		return (print_exported_vars(shell->envp));
 	while (args[i])
 	{
-		if (!ft_strchr(args[i], '='))
+		if (!is_valid_identifier(args[i]))
 		{
-			i++;
-			continue ;
-		}
-		if (add_or_update_env(args[i], &shell->envp) != 0)
-		{
-			ft_putstr_fd("minishell: export: allocation error\n", 2);
+			ft_putstr_fd("minishell: export: `", 2);
+			ft_putstr_fd(args[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
 			status = 1;
+		}
+		else
+		{
+			if (add_or_update_env(args[i], &shell->envp) != 0)
+			{
+				ft_putstr_fd("minishell: export: allocation error\n", 2);
+				status = 1;
+			}
 		}
 		i++;
 	}
